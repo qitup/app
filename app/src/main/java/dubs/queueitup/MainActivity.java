5bd04e5c66c6059d3ee5b9b2b2173d06aa91d24b
TrevorFoster <trevor.foster@hotmail.ca>
2017-10-09 17:41:49 -0400

    Added code to request JWT token after redirecting to our domain.

--- app/src/main/java/dubs/queueitup/MainActivity.java
+++ app/src/main/java/dubs/queueitup/MainActivity.java
@@ -3,6 +3,7 @@ package dubs.queueitup;
 import android.app.ActionBar;
 import android.app.FragmentTransaction;
 import android.content.DialogInterface;
+import android.graphics.Bitmap;
 import android.os.Build;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.FragmentActivity;
@@ -17,8 +18,7 @@ import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
+import android.webkit.*;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -27,9 +27,7 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
+import com.android.volley.toolbox.*;
 import com.spotify.sdk.android.authentication.*;
 import com.spotify.sdk.android.player.Config;
 import com.spotify.sdk.android.player.ConnectionStateCallback;
@@ -45,6 +43,14 @@ import org.json.JSONObject;
 
 import kaaes.spotify.webapi.android.SpotifyApi;
 import kaaes.spotify.webapi.android.SpotifyService;
+
+import android.webkit.CookieManager;
+
+import java.net.CookieHandler;
+import java.net.CookiePolicy;
+import java.net.HttpCookie;
+import java.net.URI;
+import java.util.Map;
 
 import static com.spotify.sdk.android.player.PlayerEvent.*;
 
@@ -63,6 +69,8 @@ public class MainActivity extends AppCompatActivity implements
     Button button;
     private WebView mWebview;
     private String baseURL = BuildConfig.scheme + "://" + getHost();
+    private RequestQueue requestQueue;
+    java.net.CookieManager systemCookies;
 
 
     // Request code that will be used to verify if the result comes from correct activity
@@ -103,36 +111,88 @@ public class MainActivity extends AppCompatActivity implements
 //        thirdTab.setText("Party"); // set the Text for the first Tab
 //        thirdTab.setIcon(R.drawable.ic_search_white_48dp); // set an icon for the first tab
 
-        mWebview  = new WebView(this);
-        RequestQueue queue = Volley.newRequestQueue(this);
+        CookieManager cookieManager = CookieManager.getInstance();
+
+        mWebview = new WebView(this);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            cookieManager.setAcceptThirdPartyCookies(mWebview, true);
+        } else {
+            cookieManager.setAcceptCookie(true);
+            cookieManager.setAcceptThirdPartyCookies(mWebview, true);
+        }
+
+        final WebSettings settings = mWebview.getSettings();
+        settings.setAppCacheEnabled(true);
+        settings.setBuiltInZoomControls(true);
+
+        systemCookies = new java.net.CookieManager(null, CookiePolicy.ACCEPT_ALL);
+        CookieHandler.setDefault(systemCookies);
+
+        requestQueue = Volley.newRequestQueue(this);
 
         String url = baseURL + "/auth/spotify";
-        mWebview.setWebViewClient(new WebViewClient());
+
+        mWebview.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+                String request_url = request.getUrl().toString();
+
+                if (request_url.startsWith(baseURL + "/auth/spotify/callback")) {
+                    String cookie = CookieManager.getInstance().getCookie(request.getUrl().getHost());
+
+                    if (cookie != null) {
+                        String[] parts = cookie.split("=");
+                        systemCookies.getCookieStore().add(URI.create(baseURL), new HttpCookie(parts[0], parts[1]));
+                    }
+                    MainActivity.this.finishAuthentication(request_url);
+
+                    mWebview.clearCache(true);
+
+                    mWebview.onPause();
+                    mWebview.removeAllViews();
+                    mWebview.destroyDrawingCache();
+
+                    // NOTE: This pauses JavaScript execution for ALL WebViews,
+                    // do not use if you have other WebViews still alive.
+                    // If you create another WebView after calling this,
+                    // make sure to call mWebView.resumeTimers().
+                    mWebview.pauseTimers();
+
+                    mWebview.destroy();
+                    mWebview = null;
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                super.onPageStarted(view, url, favicon);
+//                loadingFinished = false;
+                //SHOW LOADING IF IT ISNT ALREADY VISIBLE
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+//                if(!redirect){
+//                    loadingFinished = true;
+//                }
+//
+//                if(loadingFinished && !redirect){
+//                    //HIDE LOADING IT HAS FINISHED
+//                } else{
+//                    redirect = false;
+//                }
+                super.onPageFinished(view, url);
+            }
+        });
         mWebview.loadUrl(url);
         setContentView(mWebview);
 
-//        // Request a string response from the provided URL.
-//        JsonObjectRequest stringRequest = new JsonObjectRequest(Request.Method.GET, url, null,
-//                new Response.Listener<JSONObject>() {
-//                    @Override
-//                    public void onResponse(JSONObject response) {
-//                        // Display the first 500 characters of the response string.
-//                        Log.d("MainActivity", "Response is: " + response.toString());
-//                        mWebview.setWebViewClient(new WebViewClient());
-//
-//                        mWebview .loadUrl(response.toString());
-//                        setContentView(mWebview);
-//
-//                    }
-//                }, new Response.ErrorListener() {
-//            @Override
-//            public void onErrorResponse(VolleyError error) {
-//                Log.e("Error", "That didn't work!" + error.toString());
-//            }
-//        });
 //
 //        // Add the request to the RequestQueue.
-//        queue.add(stringRequest);
 //        int party = R.layout.party_page;
 //                Button button2 = (Button) party_page find(R.id.joinPartyButton);
 //        button2.setOnClickListener(new View.OnClickListener() {
@@ -142,6 +202,25 @@ public class MainActivity extends AppCompatActivity implements
 //            }
 //        });
 
+    }
+
+    private void finishAuthentication(String exchange_url) {
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, exchange_url, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        // Display the first 500 characters of the response string.
+                        Log.d("MainActivity", "Response is: " + response.toString());
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.e("Error", "That didn't work!" + error.toString());
+                    }
+                });
+
+        requestQueue.add(request);
     }
 
     @Override
