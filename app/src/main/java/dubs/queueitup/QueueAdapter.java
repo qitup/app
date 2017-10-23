2b8975e5c94c1e3ead65a4752ba0741e6b29ef35
ryanschott <ryan.schott95@gmail.com>
2017-10-23 01:57:02 -0400

    Inital queueAdapter implementation

---
+++ app/src/main/java/dubs/queueitup/QueueAdapter.java
@@ -0,0 +1,102 @@
+package dubs.queueitup;
+
+import android.content.ClipData;
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.common.base.Joiner;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
+
+import kaaes.spotify.webapi.android.models.ArtistSimple;
+import kaaes.spotify.webapi.android.models.Image;
+import kaaes.spotify.webapi.android.models.Track;
+
+/**
+ * Created by ryanschott on 2017-10-23.
+ */
+
+public class QueueAdapter extends RecyclerView.Adapter<QueueAdapter.ViewHolder> {
+
+    private final List<Track> mItems = new ArrayList<>();
+    private final Context mContext;
+    private final ItemVotedListener mListener;
+
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        public final TextView title;
+        public final TextView subtitle;
+        public final ImageView image;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            title = (TextView) itemView.findViewById(R.id.entity_title);
+            subtitle = (TextView) itemView.findViewById(R.id.entity_subtitle);
+            image = (ImageView) itemView.findViewById(R.id.entity_image);
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            notifyItemChanged(getLayoutPosition());
+            mListener.onItemVoted(v, mItems.get(getAdapterPosition()));
+        }
+    }
+
+    public interface ItemVotedListener {
+        void onItemVoted(View itemView, Track item);
+    }
+
+    public QueueAdapter(Context context, ItemVotedListener listener) {
+        mContext = context;
+        mListener = listener;
+    }
+
+    public void clearData() {
+        mItems.clear();
+    }
+
+    public void addData(List<Track> items) {
+        mItems.addAll(items);
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item, parent, false);
+        return new QueueAdapter.ViewHolder(v);
+    }
+
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        Track item = mItems.get(position);
+
+        holder.title.setText(item.name);
+
+        List<String> names = new ArrayList<>();
+        for (ArtistSimple i : item.artists) {
+            names.add(i.name);
+        }
+        Joiner joiner = Joiner.on(", ");
+        holder.subtitle.setText(joiner.join(names));
+
+        Image image = item.album.images.get(0);
+        if (image != null) {
+            Picasso.with(mContext).load(image.url).into(holder.image);
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return mItems.size();
+    }
+}
